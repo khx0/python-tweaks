5524d02743a5cc7bafe292d47362511a77a7eed7
Nikolas Schnellbächer <khx0@posteo.net>
2019-08-23 00:01:04 +0200

    added 2nd test case

--- regexp-patterns/case_insensitive_string_search.py
+++ regexp-patterns/case_insensitive_string_search.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-08-22
+# date: 2019-08-23
 # file: wrap_multiparameter_functions.py
 # tested with python 2.7.15
 # tested with python 3.7.2
@@ -18,9 +18,20 @@ import re
 
 if __name__ == '__main__':
 
+	# test 1
     full_string = 'path_to_data_set_type_c_3.dat'
 
     sub_string = 'TYPE_C'
+
+    if re.search(sub_string, full_string):
+    	print('{} contains {}.'.format(full_string, sub_string))
+
+    if re.search(sub_string, full_string, re.I):
+    	print('{} contains {} using the re.I option.'.format(full_string, sub_string))
+
+    # test 2
+    full_string = 'path_to_data_set_TYPE_c_3.dat'
+    sub_string = 'type_c'
 
     if re.search(sub_string, full_string):
     	print('{} contains {}.'.format(full_string, sub_string))
