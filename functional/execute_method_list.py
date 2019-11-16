5de1616bc51980353f9b49d582beb004d5a08530
Nikolas Schnellbächer <khx0@posteo.net>
2019-11-16 10:23:11 +0100

    added execute_method_list.py module

---
+++ functional/execute_method_list.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2019-11-16
+# file: execute_function_list.py
+# tested with python 3.7.2
+##########################################################################################
+
+class BaseClass():
+
+    def __init__(self):
+
+        self.fList = []
+        self.fList.append(self.print_01)
+        self.fList.append(self.print_02)
+        self.fList.append(self.print_03)
+
+    def print_01(self):
+        print("=== 01 ===")
+
+    def print_02(self):
+        print("=== 02 ===")
+
+    def print_03(self):
+        print("=== 03 ===")
+
+    def execute_F_stack(self):
+        
+        for Func in self.fList:
+
+            Func()
+
+if __name__ == '__main__':
+
+    class_01 = BaseClass()
+
+    class_01.execute_F_stack()
