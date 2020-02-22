d9f2c0a43e65a7ae3854eee84d6d53ff127cbd11
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-22 13:28:39 +0100

    added python internals example 01

---
+++ python-internals/python_internals_example_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2020-02-22
+# file: python_internals_example_01.py
+# tested with python 3.7.2
+##########################################################################################
+
+if __name__ == '__main__':
+
+    pass
