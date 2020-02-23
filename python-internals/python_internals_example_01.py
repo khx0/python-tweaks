488ff9a0b5bb1128221c71f1ba1f2c77817630f9
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-23 12:58:44 +0100

    added globals prints

--- python-internals/python_internals_example_01.py
+++ python-internals/python_internals_example_01.py
@@ -3,11 +3,25 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-02-22
+# date: 2020-02-23
 # file: python_internals_example_01.py
 # tested with python 3.7.2
 ##########################################################################################
 
+import numpy as np
+
 if __name__ == '__main__':
 
-    pass
+    print("__name__ =", __name__)
+    print("__file__ =", __file__)
+    print("__doc__ =", __doc__)
+    print("__package__ =", __package__)
+
+    print("//////////////////////////////////////")
+
+    print("globals() =", globals())
+
+	print("//////////////////////////////////////")
+
+	print(np.__version__)
+	print(np)
