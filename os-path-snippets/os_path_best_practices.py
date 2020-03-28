901ffe775d6038a9512f540ecd3d234672122bca
Nikolas Schnellbächer <khx0@posteo.net>
2020-03-28 10:37:24 +0100

    new os path snippets

---
+++ os-path-snippets/os_path_best_practices.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2020-03-28
+# file: os_path_best_practices.py
+# tested with python 3.7.6
+##########################################################################################
+
+import numpy as np
+
+if __name__ == '__main__':
+
+    # os.path.isfile
+    # os.path.isdir
+    # use assertions on the two above
+
+    # os.makedirs( * , exist_ok = True)
+    
+    # os.path.join
+    
+    # os.path.basename
+    # os.path.dirname
+    
+    # os.path.normpath
+    
+    # check / \ handling (Unix vs. Windows systems)
+    
