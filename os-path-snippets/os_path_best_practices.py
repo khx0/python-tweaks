e5f29c166c5bea7f62beeb1423a355de6b270d08
Nikolas Schnellbächer <khx0@posteo.net>
2020-10-18 20:37:06 +0200

    clean up maintenance

--- os-path-snippets/os_path_best_practices.py
+++ os-path-snippets/os_path_best_practices.py
@@ -3,15 +3,15 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-03-28
+# date: 2020-10-18
 # file: os_path_best_practices.py
 # tested with python 3.7.6
 ##########################################################################################
 
-import numpy as np
 
 if __name__ == '__main__':
 
+    # TODO: Use in a sentence ...
     # os.path.isfile
     # os.path.isdir
     # use assertions on the two above
---
+++ pitfalls/floating_point_issues.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2020-10-04
+# file: floating_point_issues.py
+# tested with python 3.7.6
+##########################################################################################
+
+if __name__ == '__main__':
+
+    
