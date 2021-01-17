6d697e2e80b7068e6e8a8f3b74d3e01f0107515d
Nikolas Schnellbächer <khx0@posteo.net>
2021-01-17 18:55:25 +0100

    added VSCode interactive minimal example

---
+++ interactive/VSCode_python_interactive_test.py
@@ -0,0 +1,16 @@
+#%%
+import matplotlib as mpl
+import numpy as np
+
+print("Hello ")
+
+#  %%
+# do work here ...
+
+#################################################################################
+# Working wth the Python Interactive window in VS Code
+# Jupyter-like code cells withing Python code are defined using a # %% comment.
+# For more resources on this see the link below:
+# https://code.visualstudio.com/docs/python/jupyter-support-py
+# per 2021-01-17
+#################################################################################
